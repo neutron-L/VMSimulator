7b4f1723a2590d1728290e3d23cb50436b4307ef
rda <2055017970@qq.com>
2023-04-23 00:54:38 +0800

    some pager derived class(need to implement

--- pager.cc
+++ pager.cc
@@ -1,8 +1,29 @@
 #include "pager.hh"
 
+#include <string>
+using std::ifstream;
+using std::string;
+using std::vector;
+
 uint32_t FifoPager::select_victim_frame()
 {
     auto idx = hand;
     hand = (hand + 1) % frames;
     return idx;
+}
+
+RandomPager::RandomPager(uint32_t & f, ifstream & fin) : frames(f)
+{
+    uint32_t n;
+    fin >> n;
+    random_numbers.resize(n);
+    for (auto & num : random_numbers)
+        fin >> num;
+}
+
+uint32_t RandomPager::select_victim_frame()
+{
+    uint32_t idx = ofs;
+    ofs = (ofs + 1) % random_numbers.size();
+    return random_numbers[idx] % frames;
 }
--- pager.hh
+++ pager.hh
@@ -1,7 +1,9 @@
 #ifndef PAGER_HH
 #define PAGER_HH
 
+#include <fstream>
 #include <cstdint>
+#include <vector>
 
 #include "pager.hh"
 
@@ -20,6 +22,42 @@ private:
     uint32_t frames{};
 public:
     FifoPager(uint32_t & f) : frames(f) {}
+    virtual uint32_t select_victim_frame() override;
+};
+
+class RandomPager : public Pager
+{
+private:
+    uint32_t ofs{};
+    uint32_t frames{};
+    std::vector<uint32_t> random_numbers;
+public:
+    RandomPager(uint32_t & f, std::ifstream & fin);
+    virtual uint32_t select_victim_frame() override;
+};
+
+class AgingPager : public Pager
+{
+public:
+    virtual uint32_t select_victim_frame() override;
+};
+
+
+class ESCPager : public Pager
+{
+    public:
+    virtual uint32_t select_victim_frame() override;
+};
+
+class ClockPager : public Pager
+{
+    public:
+    virtual uint32_t select_victim_frame() override;
+};
+
+class WorkingSetPager : public Pager
+{
+    public:
     virtual uint32_t select_victim_frame() override;
 };
 
